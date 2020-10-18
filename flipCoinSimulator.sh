3bde16b2735b7b97c0535da511e192774e70389a
Ankita <Ankitaangane20@gmail.com>
2020-10-18 12:16:08 +0530

    Added a tie or difference less than 3 condition

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -10,28 +10,52 @@
 h_counter=0
 t_counter=0
 diff=0
+final_counter=21
 
-while [ $h_counter -le 21 -a $t_counter -le 21 ]
+
+while [ $h_counter -le $final_counter -a $t_counter -le $final_counter ]
 do
 	result=$((RANDOM%2))
 	if [ $result -eq 0 ]
 	then
 		h_counter=$(( h_counter + 1 ))
-		if [ $h_counter -eq 21 ]
+		if [ $h_counter -eq $final_counter ]
 		then
 			echo "HEADS count : $h_counter ;  TAILS count : $t_counter"
 			diff=$(( $h_counter - $t_counter ))
 			echo "HEADS Won game by $diff times "
-			break;
+			if [ $diff -le 3 ]
+			then
+				if [ $diff -eq 0 ]
+				then
+					echo "Its a TIE as difference is $diff"
+				else
+					echo "Difference is too less, So please continue"
+					final_counter=$(( final_counter + 1 ))
+				fi
+			else
+				break;
+			fi
 		fi
 	else
 		t_counter=$(( t_counter + 1 ))
-		if [ $t_counter -eq 21 ]
+		if [ $t_counter -eq $final_counter ]
 		then
 			echo "HEADS count : $h_counter ;  TAILS count : $t_counter"
 			diff=$(( $t_counter - $h_counter ))
 			echo "TAILS Won game by $diff times "
-			break;
+			if [ $diff -le 3 ]
+                        then
+                                if [ $diff -eq 0 ]
+                                then
+                                        echo "Its a TIE as difference is $diff"
+                                else
+                                        echo "Difference is too less, So please continue"
+					final_counter=$(( final_counter + 1 ))
+                                fi
+                        else
+                                break;
+                        fi
 		fi
 	fi
 done
