3ed63d9a6b988ccc203dd71fa8e9548242874dd4
Ankita <Ankitaangane20@gmail.com>
2020-10-18 11:45:46 +0530

    Added multiple times flipping for a coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,12 +4,22 @@
 #Flip Coin Simulation Program
 
 #UC1:Flipping a cointo display head or tail as a winner
+#UC2: Added a loop for flipping 21 times
 
-result=$((RANDOM%2))
-if [ $result -eq 0 ]
-then
-	echo "HEADS is a winner"
-else
-	echo "TAILS is a winner"
-fi
+#variable declaration
+h_counter=0
+t_counter=0
 
+for(( counter=1; counter<=21; counter++ ))
+do
+	result=$((RANDOM%2))
+	if [ $result -eq 0 ]
+	then
+		h_counter=$(( h_counter + 1 ))
+	else
+		t_counter=$(( t_counter + 1 ))
+	fi
+done
+
+echo "HEADS WON $h_counter times"
+echo "TAILS WON $t_counter times"
